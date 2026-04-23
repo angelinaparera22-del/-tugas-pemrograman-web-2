cfa2d973dff53f576428bd1ac8a690a738295fd0
Stefani Parera <angelinaparera22-del.com>
2026-04-23 21:14:10 +0800

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/produk-sepatu/create.blade.php
@@ -0,0 +1,6 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h1 class="fw-bold">Form Create Sepatu</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('produk-sepatu', function () {
+    return view('produk-sepatu.index',['title' => 'Produk Sepatu']);
+});
+
+Route::get('produk-sepatu/create', function () {
+    return view('produk-sepatu.create',['title' => 'Create Produk Sepatu']);
+});
