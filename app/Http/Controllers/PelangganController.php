859bda56b1537c5eb3c78d4990338d3e259725e4
Stefani Parera <angelinaparera22-del.com>
2026-05-23 01:23:22 +0800

    feat: implementasi fungsi hapus data [Pelanggan]

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -92,6 +92,8 @@ class PelangganController extends Controller
      */
     public function destroy(Pelanggan $pelanggan)
     {
-        //
+         $pelanggan->delete();
+
+    return redirect()->route('pelanggan.index')->with('success', 'Data pelanggan berhasil dihapus!');
     }
 }
